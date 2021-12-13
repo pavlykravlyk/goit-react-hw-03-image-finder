a1f851642e9b5fedfc50d69ee200b995dd602dc2
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-12-14 00:45:22 +0100

    Delete ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++
@@ -1,9 +0,0 @@
-// import styles from './ImageGalleryItem.module.css';
-
-// export default function ImageGalleryItem({ src, alt }) {
-//   return (
-//     <li className={styles.ImageGalleryItem}>
-//       <img className={styles.ImageGalleryItem__image} src={src} alt={alt} />
-//     </li>
-//   );
-// }
